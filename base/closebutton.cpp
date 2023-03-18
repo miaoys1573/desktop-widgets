cd598889f6a1aa8bc708fb81bb95dce28210f1cc
miaoys <2228413187@qq.com>
2023-03-18 11:51:58 +0800

    统一字体颜色；修改关闭按钮图标为自定义

--- base/closebutton.cpp
+++ base/closebutton.cpp
@@ -5,19 +5,18 @@
 
 void CloseButton::initUI()
 {
-    QStyle *style = this->style();
-    QIcon icon = style->standardIcon(QStyle::SP_DialogCloseButton);
-    this->setIcon(icon);
     this->setFixedSize(Constants::CLOSE_BUTTON_HEIGHT, Constants::CLOSE_BUTTON_HEIGHT);
 }
 
 void CloseButton::loadStyleSheet()
 {
+    int padding = Constants::CLOSE_BUTTON_HEIGHT * Constants::BUTTON_PADDING / Constants::CHECK_BUTTON_HEIGHT;
     QStringList styleSheet;
-    styleSheet.append(QString("QPushButton{border-style:none;padding:%2px;border-radius:%3px;background:%4;}")
-                      .arg(Constants::BUTTON_PADDING)
+    styleSheet.append(QString("QPushButton{border-style:none;border-radius:%1px;"
+                              "background:%2;padding:3%;image:url(:/assets/icons/close.svg)}")
                       .arg(Constants::BUTTON_RADIUS)
-                      .arg(Constants::MAIN_BACKGROUND_2));
+                      .arg(Constants::MAIN_BACKGROUND_2)
+                      .arg(padding));
     styleSheet.append(QString("QPushButton:hover{background:%2;}")
                       .arg(Constants::MAIN_BACKGROUND_3));
     this->setStyleSheet(styleSheet.join(""));
--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@ int main(int argc, char *argv[])
     int fontId = QFontDatabase::addApplicationFont(":/assets/fonts/HarmonyOS_Sans_SC_Regular.ttf");
     QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
     app.setFont(QFont(fontFamily));
+    QPalette palette = app.palette();
+    palette.setColor(QPalette::WindowText, QColor(100, 100, 100));
+    app.setPalette(palette);
 
     MainSetting mainSetting;
     mainSetting.showWidgets();
